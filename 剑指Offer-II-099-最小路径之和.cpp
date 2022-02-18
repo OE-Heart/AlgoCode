cc21c08176ed2123bb95ee7845a91b9f02d4c9cf
OE.Heart <leoouyixin@126.com>
2022-02-18 16:04:27 +0800

    Complete day32&day33

---
+++ 剑指Offer-II-099-最小路径之和.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int dp[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 && j == 0) dp[i][j] = grid[0][0];
+                else if (i == 0) dp[i][j] = dp[i][j - 1] + grid[i][j];
+                else if (j == 0) dp[i][j] = dp[i - 1][j] + grid[i][j];
+                else dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
