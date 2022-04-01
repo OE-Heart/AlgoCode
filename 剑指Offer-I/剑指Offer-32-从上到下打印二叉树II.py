1f7da8bf8b731cff177aa50faa8849cf7461b838
OE.Heart <leoouyixin@126.com>
2022-04-01 15:51:01 +0800

    2022-04-01

---
+++ 剑指Offer-I/剑指Offer-32-从上到下打印二叉树II.py
@@ -0,0 +1,85 @@
+import json
+import queue
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode):
+        ret = []
+        if root is None: return ret
+        q = queue.Queue()
+        q.put(root)
+        while not q.empty():
+            level = []
+            for _ in range(q.qsize()):
+                ptr = q.get()
+                level.append(ptr.val)
+                if ptr.left: q.put(ptr.left)
+                if ptr.right: q.put(ptr.right)
+            ret.append(level)
+        return ret
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            root = stringToTreeNode(line)
+            
+            ret = Solution().levelOrder(root)
+
+            out = integerListToString(ret)
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
